95d7ae1dac3996a02093f2785051ab43accc856f
Daniel Eisterhold <deisterhold@gmail.com>
2019-08-08 15:41:20 -0500

    Fix missing method to use forwarded headers.

--- src/web/Startup.cs
+++ src/web/Startup.cs
@@ -42,6 +42,8 @@ namespace Vending.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseForwardedHeaders();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
